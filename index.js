09e69be3eb4382375b13ee2efc43dde7062028c6
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2023-08-19 22:29:43 -0600

    discount price show

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let total =0;
 
+
+
 function handleSameFunction(id){
     const idCatch= document.getElementById(id)
     const findInner= idCatch.innerText;
     const convertNumber= parseFloat(findInner)
+ 
     return convertNumber
 }
 
@@ -21,17 +24,51 @@ function handleButtonClick(target){
      const itemName = target.childNodes[1].childNodes[3].childNodes[5].innerText.split(" ")[0];
      total = parseFloat(total) + parseFloat(itemName);
      document.getElementById('total-price').innerText =total;
-     console.log(total)
-
+     const finalTotal=document.getElementById('final-price')
+     finalTotal.innerText=total;
+    //  console.log(finalTotal)
+    //  console.log(total)
+     const makePBtn= document.getElementById('make-p-btn');
+     
      if(total > 200){
-           const makePBtn= document.getElementById('make-p-btn');
+        
            makePBtn.disabled = false
      }
      else{
         makePBtn.disabled= true
      }
+    
+   
+return total
  }
-
+ 
 //  discount btn
+      
+            const totalPriceElement = document.getElementById("final-price");
+            const discountInputField = document.getElementById("discount-input");
+          const discountButton = document.getElementById("discount-btn");
+         const discountPrice= document.getElementById("discoun-price")
+
+        function disCountBtn() {
+            const discountCode =discountInputField.value; 
 
 
+            if (total>200 & discountCode === "SELL200") {
+                discountButton.disabled = false;
+              
+                const displayFinal= document.getElementById("discoun-price")
+                const text= displayFinal.innerText;
+                const convtNumber= parseFloat(text);
+                const totalAmount = total+ convtNumber;
+                const discount =( totalAmount* 0.2).toFixed(2);
+                const numberDiscountTotal = parseFloat(discount)
+                
+                displayFinal.innerText= numberDiscountTotal
+                
+              
+            } else {
+                discountButton.disabled = true;
+            }
+        }
+        discountInputField.addEventListener("input", disCountBtn);
+        discountButton.addEventListener('click', applyDiscount)
